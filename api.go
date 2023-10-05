66354900b92199e9b8a747678ebbe901b10f4ded
Shuhai Liao <shuai190060roy@gmail.com>
2023-10-05 10:07:13 +0200

    jwt for get user by ID

--- api.go
+++ api.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 
+	jwt "github.com/golang-jwt/jwt/v4"
 	"github.com/gorilla/mux"
 )
 
@@ -27,7 +29,7 @@ func (s *APIServer) Run() {
 	router.HandleFunc("/account", makeHttpHandleFunc(s.handleAccount))
 
 	// take the id as variable for http request
-	router.HandleFunc("/account/{id}", makeHttpHandleFunc(s.handleGetAccountByID))
+	router.HandleFunc("/account/{id}", withJWTAuth(makeHttpHandleFunc(s.handleGetAccountByID), s.store))
 
 	// transfer
 	router.HandleFunc("/transfer", makeHttpHandleFunc(s.handleTransfer))
@@ -96,6 +98,13 @@ func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) 
 	if err := s.store.CreateAccount(account); err != nil {
 		return err
 	}
+
+	tokenString, err := createJWT(account)
+	if err != nil {
+		return err
+	}
+	fmt.Println("jwt-token is", tokenString)
+
 	return WriteJSON(w, http.StatusOK, account)
 }
 
@@ -157,4 +166,72 @@ func getID(r *http.Request) (int, error) {
 
 	}
 	return id, nil
+}
+
+// middleware to add jwt
+func withJWTAuth(handlerFunc http.HandlerFunc, s Storage) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("calling JWT auth middleware")
+		tokenString := r.Header.Get("x-jwt-token")
+		// if len(jwt.ErrTokenNotValidYet.Error())
+		token, err := validateJWT(tokenString)
+		if err != nil {
+			WriteJSON(w, http.StatusForbidden, ApiError{Error: "invalid token-0"})
+			return
+		}
+
+		if !token.Valid {
+			WriteJSON(w, http.StatusForbidden, ApiError{Error: "invalid token-1"})
+			return
+		}
+		// -------------------------------------
+		// when the userID match with the claim number, then it can be returning the handlerFunc
+		// so it restrict  user-A can check only user-A's info, not others
+		userID, err := getID(r)
+		if err != nil {
+			WriteJSON(w, http.StatusForbidden, ApiError{Error: "invalid token-2"})
+			return
+		}
+		account, err := s.GetAccountByID(userID)
+		if err != nil {
+			WriteJSON(w, http.StatusForbidden, ApiError{Error: "invalid token-3"})
+			return
+		}
+		claims := token.Claims.(jwt.MapClaims)
+		// panic(reflect.TypeOf(claims["accountNumber"]))
+		// fmt.Println(claims, int64(claims["accountNumber"].(float64)), account.Number)
+		if account.Number != int64(claims["accountNumber"].(float64)) {
+			WriteJSON(w, http.StatusForbidden, ApiError{Error: "invalid token-4"})
+			return
+		}
+		// -------------------------------------
+		handlerFunc(w, r)
+	}
+}
+
+func validateJWT(tokenString string) (*jwt.Token, error) {
+	secret := os.Getenv("JWT_SECRET")
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte(secret), nil
+	})
+}
+
+func createJWT(account *Account) (string, error) {
+	// Create the Claims
+	claims := &jwt.MapClaims{
+		"expiresAt":     15000,
+		"accountNumber": account.Number,
+	}
+
+	secret := os.Getenv("JWT_SECRET")
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(secret)) // pass the secret in as signing key
+
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/shuai1900/gRPC_microservice
 go 1.21.1
 
 require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 )
